73ba94da986c939af682d3af0a20c854e9d8bc71
[baruc] <[barucgs@gmail.com]>
2023-01-12 13:15:25 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <h1>subir archivos</h1>
+  <hr>
+  <div class="mt-5 input-group mb-3">
+    <input type="file" class="form-control" id="inputGroupFile02">
+    <label class="input-group-text" for="inputGroupFile02">Upload</label>
+  </div>
+  <div id="btn-subir"></div>
+`;
+
+document.querySelector('#btn-subir').innerHTML = `
+  <button class="btn btn-primary" type="submit">Button</button>
+`
+
+// setupCounter(document.querySelector('#counter'))
